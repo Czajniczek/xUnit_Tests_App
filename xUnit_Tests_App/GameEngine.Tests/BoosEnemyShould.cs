b29975a3556400756a31347853dd5e7ed005f367
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-08-05 13:38:12 +0200

    Asserting on Floating Point Values
    Asserting on Floating Point Values

---
+++ xUnit_Tests_App/GameEngine.Tests/BoosEnemyShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BoosEnemyShould
+    {
+        #region Asserting on Floating Point Values
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
+        }
+        #endregion
+    }
+}
