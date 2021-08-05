982c74a2b69d211110d6d54bdbf6bb970a39d458
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-08-05 12:36:52 +0200

    Making Assertions Against String Values
    Making Assertions Against String Values

--- xUnit_Tests_App/GameEngine.Tests/PlayerCharacterShould.cs
+++ xUnit_Tests_App/GameEngine.Tests/PlayerCharacterShould.cs
@@ -5,6 +5,7 @@ namespace GameEngine.Tests
 {
     public class PlayerCharacterShould
     {
+        #region Adding an Assert to the First Test
         // Postaæ gracza powinna nie mieæ doœwiadczenia, gdy jest tworzona
         [Fact]
         public void BeInexperiencedWhenNew()
@@ -13,5 +14,75 @@ namespace GameEngine.Tests
 
             Assert.True(sut.IsNoob);
         }
+        #endregion
+
+        #region Making Assertions Against String Values
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Equal("Sarah Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameStartingWithFirstName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.StartsWith("Sarah", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndingWithLastName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            //sut.FirstName = "XXX";
+            sut.LastName = "Smith";
+
+            Assert.EndsWith("Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCaseAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "SARAH";
+            sut.LastName = "SMITH";
+
+            Assert.Equal("Sarah Smith", sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CalculateFullName_SubstringAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Contains("ah Sm", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            // Pierwsza du¿a litera imienia i pierwsza du¿a litera nazwiska
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+        }
+        #endregion
     }
 }
