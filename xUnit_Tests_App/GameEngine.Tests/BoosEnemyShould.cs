cd52a6851aae464faedfe83208a57c72478bbc1a
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-08-08 15:20:57 +0200

    Writing Custom Test Output Messages
    Writing Custom Test Output Messages

--- xUnit_Tests_App/GameEngine.Tests/BoosEnemyShould.cs
+++ xUnit_Tests_App/GameEngine.Tests/BoosEnemyShould.cs
@@ -4,16 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GameEngine.Tests
 {
     public class BoosEnemyShould
     {
+        private readonly ITestOutputHelper _output;
+
+        public BoosEnemyShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         #region Asserting on Floating Point Values
         [Fact]
         [Trait("Category", "Boss")]
         public void HaveCorrectPower()
         {
+            //Console.WriteLine("Creating Boss Enemy");
+            _output.WriteLine("Creating Boss Enemy");
+
             BossEnemy sut = new BossEnemy();
 
             // 1000÷6 = ~166.666666667
